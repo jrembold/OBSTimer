30f599e22d7b0dd177429cc322810746d521cc0d
Jed Rembold <jedediah.r@gmail.com>
2022-01-22 22:29:59 -0800

    Adjust so timer.txt always in local directory
    Previously, if the Python script was run from an alternative location,
    it would create the timer.txt file in that outside location. It will now
    always create the text file in the same directory as where the script
    exists, no matter where it is run from.

--- obs_timer.py
+++ obs_timer.py
@@ -7,12 +7,16 @@ use in an OBS scene.
 import time
 import datetime
 from dateutil.parser import parse
+import os
 import argparse
 
 parser = argparse.ArgumentParser(description="Start a countdown timer")
 parser.add_argument('-t', dest='target', default=False, action="store_true", help="Countdown to target time")
 parser.add_argument('TIME', type=str, help="Enter a floating number of minutes")
 args = vars(parser.parse_args())
+
+local_dir = os.path.dirname(os.path.realpath(__file__))
+print(local_dir)
 
 if args['target']:
     target = parse(args['TIME'])
@@ -25,6 +29,6 @@ while datetime.datetime.now() < target:
     minutes = int(remaining / 60)
     seconds = int(remaining) % 60
     print(f"   {minutes}:{seconds:02d}", end='\r')
-    with open('timer.txt', 'w') as f:
-        f.write(f"{minutes}:{seconds:02d}")
+    with open(os.path.join(local_dir, 'timer.txt'), 'w') as f:
+        f.write(f"{minutes:}:{seconds:02d}")
     time.sleep(1)
